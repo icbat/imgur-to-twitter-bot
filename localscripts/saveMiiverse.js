3ea973784dc051efff7f9719c4fa80f8e28aca3f
Evan Cobb <cobb.evan@gmail.com>
2017-03-04 14:13:26 -0600

    Cleaning up miiverse downloading script

--- localscripts/saveMiiverse.js
+++ localscripts/saveMiiverse.js
@@ -2,7 +2,4 @@
 // Paste that array into saveurls.py to get local copies
 
 var x = $('div.album-dialog > .dialog-inner > .window > .window-body > .img-wrapper > img');
-var y = x.map((a, b) => {return b.src;});
-y.each((a, b) => {
-    window.open(b, '_blank');
-});
+x.map((a, b) => {return b.src;});
